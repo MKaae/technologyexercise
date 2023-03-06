5594820e7f82dede2cb2cf5b6d564b278386a831
Xarantaur <115454990+Xarantaur@users.noreply.github.com>
2023-03-06 14:00:06 +0100

    fjolle mathias

--- src/main/java/dk/technologyexercise/controller/OurControllerø.java
+++ src/main/java/dk/technologyexercise/controller/OurControllerø.java
@@ -21,4 +21,8 @@ public class OurControllerø {
     public String atgaapaalokum(){
         return "prøv med cream";
     }
+    @GetMapping
+    public String viskalaltsåpålokummathis(){
+        return "lokum";
+    }
 }
